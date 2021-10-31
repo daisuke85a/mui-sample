3050b31a9348d279e5905724d7f1c4a82331a3b1
Daisuke Saito <dsaito85a@gmail.com>
2021-10-31 21:32:55 +0900

    [WIP] CircleChartCardの追加途中

--- pages/minimal-dashboard.tsx
+++ pages/minimal-dashboard.tsx
@@ -30,7 +30,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
 import { orange, indigo, green } from '@mui/material/colors';
 import { useReducer, useState } from 'react';
-import { BarChart, Bar } from 'recharts';
+import { BarChart, Bar, PieChart, Pie, Sector, Cell } from 'recharts';
 
 // declare module '@mui/material/styles' {
 //   interface Theme {
@@ -120,6 +120,8 @@ export default function MinimalDashboard() {
       </Box>
     );
   };
+
+  const circleChartCardColors = [green[50], green[300], green[600], green[900]];
 
   return (
     <ThemeProvider theme={theme}>
@@ -216,10 +218,19 @@ export default function MinimalDashboard() {
               />
             </Grid>
           </Grid>
+          <Grid container>
+            <Grid item xs={8}>
+              <CircleChartCard
+                title='Current Download'
+                data={CURRENT_DOWNLOAD_DATA}
+                colors={circleChartCardColors}
+              />
+            </Grid>
+          </Grid>
         </Stack>
         <Stack spacing={2} margin={2}>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={8}>
               <Button variant='outlined' onClick={handleClick}>
                 Open success snackbar
               </Button>
@@ -326,6 +337,140 @@ const BarChartCard = ({ title, color, data, trend, mainData }: BarChartCardProps
   );
 };
 
+interface CircleChartCardProps {
+  title: string;
+  data: any[];
+  colors: string[];
+}
+
+const CircleChartCard = ({ title, data, colors }: CircleChartCardProps) => {
+  const [state, setState] = useState({
+    activeIndex: 0,
+  });
+
+  const onPieEnter = (_, index) => {
+    setState({
+      activeIndex: index,
+    });
+  };
+
+  return (
+    <Card sx={{ display: 'block' }}>
+      <Box padding={2} flexGrow={1}>
+        <Typography
+          variant='h6'
+          sx={{
+            margin: '0px',
+            fontWeight: 600,
+            lineHeight: 1.57143,
+            fontSize: '0.875rem',
+          }}
+        >
+          {title}
+        </Typography>
+      </Box>
+      <Box display='flex' alignItems='center' justifyContent='center'>
+        {/* <ResponsiveContainer> */}
+        <PieChart width={800} height={400}>
+          <Pie
+            activeIndex={state.activeIndex}
+            activeShape={renderActiveShape}
+            data={data}
+            cx='50%'
+            cy='50%'
+            innerRadius={110}
+            outerRadius={120}
+            paddingAngle={2}
+            fill='#8884d8'
+            dataKey='value'
+            onMouseEnter={onPieEnter}
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+        {/* </ResponsiveContainer> */}
+      </Box>
+    </Card>
+  );
+};
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
+  return (
+    <g fontFamily={['Public Sans', 'sans-serif'].join(',')}>
+      <text fontSize='0.875rem' x={cx} y={cy - 16} dy={8} textAnchor='middle' fill='#637381'>
+        {payload.name}
+      </text>
+      <text
+        fontSize='1.5rem'
+        fontWeight='700'
+        x={cx}
+        y={cy + 12}
+        dy={8}
+        textAnchor='middle'
+        fill='#333'
+      >
+        {value}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill='none' />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke='none' />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={6}
+        textAnchor={textAnchor}
+        fill='#333'
+      >{`${payload.name} ${value}`}</text>
+      {/* <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill='#999'>
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text> */}
+    </g>
+  );
+};
+
 const activeUserData = [
   {
     name: 'Page A',
@@ -370,3 +515,12 @@ const activeUserData = [
     amt: 2100,
   },
 ];
+
+const CURRENT_DOWNLOAD_DATA = [
+  { name: 'Mac', value: 12244 },
+  { name: 'Window', value: 53345 },
+  { name: 'iOS', value: 44313 },
+  { name: 'Android', value: 78343 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
